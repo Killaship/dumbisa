64640b10d20a3c08eacd4f32f762c40fdad982f3
Killaship <69988679+Killaship@users.noreply.github.com>
2022-03-12 21:59:37 -0500

    Update main.c

--- src/main.c
+++ src/main.c
@@ -12,7 +12,24 @@ int stack[64];
 #define sp (registers[sp])
 #define ip (registers[ip])
 
+const int prgm {
+	push, 2,
+	push, 2,
+	add,
+	pop,
+	hlt
+}
+
+int fetch() {
+	return prgm[ip];
+}
+
+void execute(int instruction) {
+	
+}
+
 void main() {
+	sp = -1;
 	while(run) {
 		execute(fetch());
 		ip++;
