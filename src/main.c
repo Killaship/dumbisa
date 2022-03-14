87945d14eaedc0eb2d523f0add5c7c309b323817
Killaship <69988679+Killaship@users.noreply.github.com>
2022-03-13 20:44:40 -0400

    Update main.c

--- src/main.c
+++ src/main.c
@@ -7,7 +7,7 @@ static int registers[numregisters];
 typedef enum {
 	push, pop, add, hlt
 	} isa;
-bool run = true;
+bool running = true;
 int stack[64];
 
 #define sp (registers[stp])
@@ -38,7 +38,7 @@ void execute(int instruction) {
 int main(void) {
 	sp = -1;
 	ip = 0;
-	while(run) {
+	while(running) {
 		execute(fetch());
 		ip++;
 	}
